9920d15f3cd281dd97c528933af548e714f930b9
elikolarova05 <elikolarova136@gmail.com>
2022-12-16 22:32:29 +0200

    Fix js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,14 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  let hotPrice = document.querySelectorAll(".price");
+
+  for (let i = 0; i < hotPrice.length; i++){
+      if(hotPrice[i].classList.contains('hot')){
+          hotPrice[i].append("🔥");
+      }
+  }
+});
